ad10f3bcde0e154d95cb9f34f0d2fea6172a463b
Gary Kim <gary@garykim.dev>
2024-03-11 15:16:13 -0400

    Add small tags

--- ulab-asset-tag-generator.py
+++ ulab-asset-tag-generator.py
@@ -24,11 +24,11 @@ from PIL import Image, ImageDraw, ImageFont
 import qrcode
 
 
-def _generate_asset_qr(asset_id: str) -> Image:
+def _generate_asset_qr(asset_id: str, box_size=40) -> Image:
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_M,
-        box_size=40,
+        box_size=box_size,
         border=0,
     )
 
@@ -63,21 +63,35 @@ def _main():
     if not args.save.is_dir():
         raise Exception('Given path does not seem to be a directory')
 
-    unicode_font = ImageFont.truetype('LiberationSans-Bold.ttf', 300)
-    size = (32 * 96, 9 * 96)
+    unicode_font_large = ImageFont.truetype('LiberationSans-Bold.ttf', 300)
+    unicode_font_small = ImageFont.truetype('LiberationSans-Bold.ttf', 80)
+    size_large = (32 * 96, 9 * 96)
+    size_small = (9 * 96, 9 * 96)
 
     for i in range(args.tags_from, args.tags_to + 1):
         number = f'{i:05}'
-        im = Image.new('1', size, (1,))
+        im_large = Image.new('1', size_large, (1,))
 
-        d = ImageDraw.Draw(im)
+        d_large = ImageDraw.Draw(im_large)
 
-        d.text((1 * 96, 75), 'μLab Asset', font=unicode_font, fill=(0,))
-        d.text((1 * 96, 475), number, font=unicode_font, fill=(0,))
+        d_large.text((1 * 96, 75), 'EE μLab Asset', font=unicode_font_large,
+                     fill=(0,))
+        d_large.text((1 * 96, 475), number, font=unicode_font_large, fill=(0,))
         qr = _generate_asset_qr(number)
-        im.paste(qr, (2050, 10))
+        im_large.paste(qr, (2050, 10))
 
-        im.save(args.save / f'{i}.png')
+        im_large.save(args.save / f'{i}.png')
+
+        # Generate small image
+        im_small = Image.new('1', size_small, (1,))
+
+        d_small = ImageDraw.Draw(im_small)
+
+        d_small.text((1 * 96, 10), f'EE μLab {number}',
+                     font=unicode_font_small, fill=(0,))
+        im_small.paste(_generate_asset_qr(number, 32), (1 * 96, 150))
+
+        im_small.save(args.save / f'{i}_small.png')
 
 
 if __name__ == '__main__':
